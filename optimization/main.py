395d4ac671eb06299257efc8d7e055b9b78108a3
Marcus <88283829+PyMarcus@users.noreply.github.com>
2024-01-29 17:21:57 -0800

    hill climb: optimization algorithm

--- optimization/main.py
+++ optimization/main.py
@@ -1,7 +1,7 @@
 # heuristics are specifics
 # metaheuristics are not specifics
-import sys
 import typing
+import mlrose
 
 # Problem: Find the minimum cost of the ticket to italy
 peoples = [('Lisboa', 'LIS'),
@@ -40,8 +40,36 @@ def get_price(schedule: typing.List[int]) -> None:
     print(f"Total Price: ${price}")
 
 
+def fitness_function(schedule: typing.List[int]) -> int:
+    """Função de otimizacao que resulta no menor valor de passagens possível"""
+    flight_id: int = -1
+    price: int = 0
+    for index in range(len(schedule) // 2):
+        origin = peoples[index][1]
+        flight_id += 1
+        flight_to = flights[(origin, destiny)][schedule[flight_id]]
+        price += flight_to[2]
+        flight_id += 1
+        flight_from = flights[(destiny, origin)][schedule[flight_id]]
+        price += flight_from[2]
+    return price
+
+
 if __name__ == '__main__':
     # formato de solucao. Num pessoas * 2, pois sao ida e volta
     # a cada posicao indica a cidade, [ida,volta]
+
     schedule_simulation = [1, 0, 3, 2, 7, 3, 6, 3, 2, 4, 5, 3]
     get_price(schedule_simulation)
+    fitness = mlrose.CustomFitness(fitness_function)  # a funcao customfitness exige uma ja personalizada para fazer as combinacoes
+    problem = mlrose.DiscreteOpt(length=12, fitness_fn=fitness, maximize=False, max_val=10) # 0 - 9
+    # Algoritmo hill climb, encontra de forma randomica uma solucao
+    # com base nessa solucao, vai para o menor ponto de curvatura
+    # mas, não é garantido que será a menor curvatura possivel
+    # (max e minimos locais ou globais)
+    best_solution, best_cost = mlrose.hill_climb(problem)
+    print(get_price(best_solution))
+    # Algoritmo de tempera simulada (pode buscar pontos piores, na esperança de achar valores melhores)
+    best_solution, best_cost = mlrose.simulated_annealing(problem)
+    print(get_price(best_solution))
+    
